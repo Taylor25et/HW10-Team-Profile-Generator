d13fbd8e34947582bcebb5e7b30cfa1790c32571
Taylor25et <taylor25et@gmail.com>
2022-03-06 19:25:40 -0800

    getting tests to work

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,11 @@
-const Employee = require('../lib/Employee')
+const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof e).toBe("object");
+});
+test("Can get name via getName()", () => {
+  const testValue = "Amanda";
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe(testValue);
+});
